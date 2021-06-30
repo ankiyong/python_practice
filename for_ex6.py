bde90b2347459ec907e5f9bf755bab02a7ae076f
ankiyong <pop2413@gmail.com>
2021-06-30 20:27:00 +0900

    Daily Python6

--- for_ex6.py
+++ for_ex6.py
@@ -1,22 +1,29 @@
-# for i in range(0,4):
-#     for j in range(0,5):
-#         print('*',end=" ")
-#     print()
-#
-# for i in range(0,6):
-#     for j in range(0,i):
-#         print('*',end=" ")
-#     print()
-#
-# for i in range(0,5):
-#     for j in range(0,5-i):
-#         print('*',end=" ")
-#     print()
-#
-
-for i in range(1,6):
-    for j in range(0,9):
-
+for i in range(0,4):
+    for j in range(0,5):
         print('*',end=" ")
+    print()
 
+for i in range(0,6):
+    for j in range(0,i):
+        print('*',end=" ")
+    print()
+
+for i in range(0,5):
+    for j in range(0,5-i):
+        print('*',end=" ")
+    print()
+
+for i in range(0,5):
+    for j in range(0,i):
+        print(' ',end=" ")
+    for k in range(9,i*2,-1):
+        print('*',end=" ")
+    print()
+
+
+for i in range(0,5):
+    for j in range(4,i,-1):
+        print(' ',end=" ")
+    for k in range(0,i*2+1):
+        print('*',end=" ")
     print()
