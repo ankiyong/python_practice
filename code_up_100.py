cc257f5b704f29a1eec14f762de42ba7f51a0c5d
ankiyong <pop2413@gmail.com>
2021-07-11 18:33:34 +0900

    Daily Python10

---
+++ code_up_100.py
@@ -0,0 +1,76 @@
+# #1
+# print('hello')
+#
+# #2
+# print('Hello World')
+#
+# #3
+# print('Hello\n'
+#       'World')
+#
+# #4
+# print("'Hello'")
+#
+# #5
+# print('"Hello World"')
+#
+# #6
+# print('"!@#%$^&*()\'')
+#
+# #7
+# print('"C:\Download\\\'hello\'.py"')
+#
+# #8
+# print('print("Hello\\nWorld")')
+#
+# #9
+# a = input()
+# print(a)
+#
+# #10
+# n = int(input())
+# print(n)
+#
+# #11
+# a_2 = float(input('입력 : '))
+# print('출력 : %f' % a_2)
+#
+# #12
+# #모름
+#
+# #13
+# d = input()
+# e = input()
+# print(e)
+# print(d)
+#
+# #14
+# a_3 = float(input('입력 : '))
+# print('출력 : %.2f' % a_3
+#
+# #15
+# a,b = input().split()
+# print(a)
+# print(b)
+#
+# #16
+# a,b = input().split()
+# print(b)
+# print(a)
+#
+# #17
+# a = input()
+# print(a,a,a)
+
+#18
+# z,x = input().split(':')
+# print('%s:%s' % (z,x))
+
+#19
+# y,m,d = input().split('.')
+# print('%s-%s-%s' % (d,m,y))
+
+#20
+a,b = input().split('-')
+print(a,end="")
+print(b)
