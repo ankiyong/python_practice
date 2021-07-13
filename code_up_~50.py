53a3d9d26749c30742f4cba1b24f3405a731fada
ankiyong <pop2413@gmail.com>
2021-07-13 21:45:03 +0900

    Daily Python12

---
+++ code_up_~50.py
@@ -0,0 +1,41 @@
+# #32
+# n = int(input())
+# print(-n)
+
+#33
+# a = input()
+# print(a+1)
+#
+# #34
+# a,b = input().split()
+# c = int(a) - int(b)
+# print(c)
+#
+# #35
+# f1,f2 = input().split()
+# c = float(f1) * float(f2)
+# print(c)
+#
+# #36
+# w,a = input().split()
+# print(w*int(a))
+
+#37
+# n = int(input())
+# s = input()
+# print(s*n)
+
+#38
+# a,b = input().split()
+# c = int(a)**int(b)
+# print(c)
+
+#39
+# f1, f2 = input().split()
+# c = float(f1)**float(f2)
+# print(c)
+
+#40
+a,b = input().split()
+c = int(a)//int(b)
+print(c)
