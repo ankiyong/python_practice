0e22858b478ea66eaf5aa1a9bc3e1ca20bf195cf
ankiyong <pop2413@gmail.com>
2021-07-14 21:05:33 +0900

    Daily Python13

--- code_up_~50.py
+++ code_up_~50.py
@@ -36,6 +36,51 @@
 # print(c)
 
 #40
-a,b = input().split()
-c = int(a)//int(b)
-print(c)
+# a,b = input().split()
+# c = int(a)//int(b)
+# print(c)
+
+#41
+# a,b = input().split()
+# c = int(a)%int(b)
+# print(c)
+
+#42
+# a = float(input())
+# print('%.2f' % a)
+
+#43
+# f1,f2 = input().split()
+# a = float(f1) / float(f2)
+# print('%.3f' % a)
+
+#44
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+# print('%d' % (a+b))
+# print('%d' % (a-b))
+# print('%d' % (a*b))
+# print('%d' % (a//b))
+# print('%d' % (a%b))
+# print('%.2f' % (a/b))
+
+#45
+# a, b, c = input().split()
+# total = int(a)+int(b)+int(c)
+# avg = ('%.2f' % (total/3))
+# print(total,avg)
+
+#46
+# n = int(input())
+# print(n<<1)
+
+#47
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+# print(a * 2**b)
+
+#48
+# a,b = input().split()
+# print(a<b)
--- for 연습문제.py
+++ for 연습문제.py
@@ -14,10 +14,10 @@
 #     print(i,end=" ")
 # print('발사')
 #
-# for i in range(0,4):
-#     for j in range(0,5):
-#         print('*',end=" ")
-#     print()
+for i in range(0,4):
+    for j in range(0,5):
+        print('*',end=" ")
+    print()
 # for i in range(0,5):
 #     for j in range(0,i+1):
 #         print('*',end=" ")
@@ -40,17 +40,17 @@
 #     for k in range(9,i*2,-1):
 #         print('*',end=" ")
 #     print()
-money = 10000
-song = 2000
-sing = 0
-change = money - (song * sing)
-while sing < 6:
-
-    if change != 0:
-        print('노래를 %d곡 불렀습니다.' % sing)
-        print('현재 %d원 남았습니다.' % (change))
-    else:
-        break
-    sing += 1
-    money = money-song
-print('잔액이 없습니다. 종료합니다.')
+# money = 10000
+# song = 2000
+# sing = 0
+# change = money - (song * sing)
+# while sing < 6:
+#
+#     if change != 0:
+#         print('노래를 %d곡 불렀습니다.' % sing)
+#         print('현재 %d원 남았습니다.' % (change))
+#     else:
+#         break
+#     sing += 1
+#     money = money-song
+# print('잔액이 없습니다. 종료합니다.')
