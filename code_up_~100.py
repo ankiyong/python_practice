d94cabf8e9e01854376a57797f85b050b0536694
ankiyong <pop2413@gmail.com>
2021-07-30 09:00:09 +0900

    Daily Python22

--- code_up_~100.py
+++ code_up_~100.py
@@ -1,9 +1,9 @@
-#91
-a,b,c = map(int,input().split())
-day = 1
-while day%a!=0 or day%b!=0 or day%c!=0:
-    day+=1
-print(day)
+# #91
+# a,b,c = map(int,input().split())
+# day = 1
+# while day%a!=0 or day%b!=0 or day%c!=0:
+#     day+=1
+# print(day)
 
 #92
 # n = int(input())
@@ -44,5 +44,6 @@ print(day)
 #         baduk[i][y] = 1 if baduk == 0 else 1
 # for s in baduk:
 #    print(*s)
+#
 
-#99
+
